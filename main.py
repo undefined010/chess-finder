3b758e5dc8ab5705506e6f532e0544e4f73ba77e
Ahmad Odeh <ahmadnodeh2007@icloud.com>
2023-02-04 21:45:38 +0300

    fixing bugs and adding some static methods to src/chess_com

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def show_options() -> None:
     for option in options : print(clr.GREEN + option)
 
     
+def save_data(data : dict) -> None:
+    user: str = input(clr.BLUE + "Do you want to save data ?\n1)yes\n2)no\n=> ")
+    
+    if user.upper() == 'YES':
+        fname : str = input(clr.GREEN + 'enter a name to txt file to save it or file path : ')
+        GetChessInfo.save_output(fname,data)
+        
+    if user.upper() == 'NO':
+        print(clr.BLUE + 'ok!')
 
 def main() -> None:
     print(clr.RED + pf.figlet_format('Chess Finder'))
@@ -24,19 +33,21 @@ def main() -> None:
                 chess_com_username : str = input(clr.RED + 'Enter a chess.com username : ')
                 data : dict = GetChessInfo.get_player_profile(chess_com_username)
                 for val,key in data.items(): print(clr.BLUE + '{} {}'.format(val,key))
+                save_data(list(data.keys()) + list(data.values()))
 
 
             elif int(option_input) == 2:
-                # chess_com_username in this case is usless
                 t : str = input(clr.GREEN + 'Enter a title in chess.com format ex Grand Master (GM): ')
                 for data in GetChessInfo.get_players_by_title(t): print(clr.BLUE + data)
-    
+                save_data(GetChessInfo.get_players_by_title(t))
+                
             elif int(option_input) == 3:
                 data : dict = GetChessInfo.get_online_streamers()
                 for val,key in data.items(): print(clr.BLUE + '{} {}'.format(val,key))
+                save_data(list(data.values()))
 
         except KeyboardInterrupt:
-            print (clr.RED + '\nGoodbye !')
+            print (clr.RED + '\nGoodbye !',end='\n')
             break
 
     
--- src/chess_com.py
+++ src/chess_com.py
@@ -1,55 +1,80 @@
-import requests,json
+import requests,json,datetime
 
+"""
+    converty time stamps into readable time format
+"""
+
+def get_req(url,err_msg):
+    r = requests.get(url)
+
+    if r.status_code != 200 :
+        print (msg)
+        return None
+        
+    return r.content
 
 class GetChessInfo:
     URL = 'https://api.chess.com/pub/'
     
     @staticmethod
     def get_player_profile(username : str) -> dict:
-        r = requests.get(GetChessInfo.URL + 'player/'+ username)
-
-        if r.status_code != 200 :
-            print ('player may not exist or an error occurred')
-            return None
-
-        return json.loads(r.content)
+        return json.loads(get_req(GetChessInfo.URL + 'player/'+ username,'player may not found'))
     
     @staticmethod
     def get_players_by_title(title : str) -> list:
-        r = requests.get(GetChessInfo.URL + 'titled/'+title)
-
-        if r.status_code != 200 :
-            print ('title may not exist or an error occurred')
-            return None
-        
-        return json.loads(r.content)['players']
-
+        return json.loads(get_req(GetChessInfo.URL + 'titled/'+title,'title may not exist or an error occurred'))['players']
+    
+    
     @staticmethod
-    def get_player_country(username : str) -> str:
-        r = requests.get(GetChessInfo.URL + 'player/'+ username)
-
-        if r.status_code != 200 :
-            print ('player may not exist or an error occurred')
-            return None
+    def get_players_by_country(iso : str) -> list:
+        # max is 10000 from chess.com 
+        return json.loads(get_req(GetChessInfo.URL + 'country/{iso}/players','cheak the name'))['players']
+    
+    
+    @staticmethod
+    def get_clubs_by_country() -> list:
+        return json.loads(get_req(GetChessInfo.URL + 'country/clubs','cheak the name'))['clubs']
         
-        return json.loads(r.content)['country']
-        
+    @staticmethod
+    def get_country_chess_com_name() -> str:
+        return json.loads(get_req(GetChessInfo.URL + 'country/','error'))['name']
+    
     @staticmethod
     def get_online_streamers() -> None:
-        r = requests.get(GetChessInfo.URL + 'streamers/')
-
-        if r.status_code != 200 :
-            print ('streamer may not exist or an error occurred')
-            return None
-
-        data : list[dict] = json.loads(r.content)['streamers']
+        
+        data : list[dict] = json.loads(get_req(GetChessInfo.URL + 'streamers/','error'))['streamers']
         online_streamers : dict = dict()
         
         for streamer in data:
             if streamer['is_live']:
                 online_streamers[streamer['username']] = streamer['twitch_url']
-
+        
         return online_streamers
+        
+    @staticmethod
+    def save_output(fname : str, data : dict) -> None:
+        if data == None:
+            print(clr.RED + 'No data')
+            
+        with open(f'{fname}.txt','a+') as af:
+            for val , key in data.items():
+                af.write(f'{str(key)} : {str(val)} \n')
+                af.close()
+    
+    @staticmethod   
+    def save_output(fname : str, data : list) -> None:
+        
+        index : int = 0
+        
+        if data == None:
+            print(clr.RED + 'No data')
+            
+        with open(f'{fname}.txt','a+') as af:
+            counter += 1
+            af.write(f'time : {datetime.datetime.now} \n')
+            for info in data:
+                af.write(f'{index}) {str(info)} \n')
+                af.close()
                 
     
     
