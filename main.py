d261f27009efcd480a4eb01497cf4f5219fd0fc5
Ahmad Odeh <ahmadnodeh2007@icloud.com>
2023-02-09 13:21:51 +0300

    fixing bugs and converting unix time stamps to data

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src.chess_com import GetChessInfo
+from src.chess_com import GetChessInfo,clr
 import pyfiglet as pf
-from colorama import Fore as clr
+
 
 def show_options() -> None:
-    options : list[str] = ['1) Get player profile','2) Get players by titled','3) Get online streamers']
+    options : list[str] = ['1) Get player profile','2) Get players by titled','3) Get online streamers','4) Get players by country','5) Get country name in chess.com','6) Get chess clubs by country']
     for option in options : print(clr.GREEN + option)
 
     
@@ -38,13 +38,38 @@ def main() -> None:
 
             elif int(option_input) == 2:
                 t : str = input(clr.GREEN + 'Enter a title in chess.com format ex Grand Master (GM): ')
-                for data in GetChessInfo.get_players_by_title(t): print(clr.BLUE + data)
-                save_data(GetChessInfo.get_players_by_title(t))
+                data : list = GetChessInfo.get_players_by_title(t)
+                for d in data: print(clr.BLUE + d)
+                save_data(data)
                 
             elif int(option_input) == 3:
                 data : dict = GetChessInfo.get_online_streamers()
                 for val,key in data.items(): print(clr.BLUE + '{} {}'.format(val,key))
                 save_data(list(data.values()))
+
+            elif int(option_input) == 4:
+                iso  : str  = input(clr.GREEN + 'Enter a country name like Italy IT : ')
+                data : list = GetChessInfo.get_players_by_country(iso)
+                for d in data : print(clr.BLUE + d)
+                save_data(data)
+
+            elif int(option_input) == 5:
+                iso  : str  = input(clr.GREEN + 'Enter a country name like Italy IT : ')
+                data : str = GetChessInfo.get_country_name_chess_com_name(iso)
+                print(clr.BLUE + data)
+                save_data(data)
+
+            elif int(option_input) == 5:
+                iso  : str  = input(clr.GREEN + 'Enter a country name like Italy IT : ')
+                data : str = GetChessInfo.get_clubs_by_country(iso)
+                print(clr.BLUE+data)
+                save_data(data)
+
+            elif int(option_input) == 6:
+                iso  : str  = input(clr.GREEN + 'Enter a country name like Italy IT : ')
+                data : list = GetChessInfo.get_clubs_by_country(iso)
+                for d in data : print(clr.BLUE + d)
+                save_data(data)
 
         except KeyboardInterrupt:
             print (clr.RED + '\nGoodbye !',end='\n')
--- src/chess_com.py
+++ src/chess_com.py
@@ -1,14 +1,13 @@
 import requests,json,datetime
+from colorama import Fore as clr
+import datetime
 
-"""
-    converty time stamps into readable time format
-"""
 
-def get_req(url,err_msg):
+def get_req(url : str,err_msg : str):
     r = requests.get(url)
 
     if r.status_code != 200 :
-        print (msg)
+        print (err_msg)
         return None
         
     return r.content
@@ -18,29 +17,34 @@ class GetChessInfo:
     
     @staticmethod
     def get_player_profile(username : str) -> dict:
-        return json.loads(get_req(GetChessInfo.URL + 'player/'+ username,'player may not found'))
+        temp = dict()
+        data = json.loads(get_req(GetChessInfo.URL + 'player/'+ username,'player may not found'))
+        temp['last_online'] = datetime.datetime.utcfromtimestamp(int(data['last_online'])).strftime('%Y-%m-%d %H:%M:%S')
+        temp['joined']      = datetime.datetime.utcfromtimestamp(int(data['joined'])).strftime('%Y-%m-%d %H:%M:%S')
+
+        data.update(temp)
+        return dict(data)
     
     @staticmethod
     def get_players_by_title(title : str) -> list:
-        return json.loads(get_req(GetChessInfo.URL + 'titled/'+title,'title may not exist or an error occurred'))['players']
+        return list(json.loads(get_req(GetChessInfo.URL + 'titled/'+title,'title may not exist or an error occurred'))['players'])
     
     
     @staticmethod
     def get_players_by_country(iso : str) -> list:
         # max is 10000 from chess.com 
-        return json.loads(get_req(GetChessInfo.URL + 'country/{iso}/players','cheak the name'))['players']
-    
+        return json.loads(get_req(f'https://api.chess.com/pub/country/{iso}/players','error'))['players']
     
     @staticmethod
-    def get_clubs_by_country() -> list:
-        return json.loads(get_req(GetChessInfo.URL + 'country/clubs','cheak the name'))['clubs']
+    def get_clubs_by_country(iso) -> list:
+        return json.loads(get_req(GetChessInfo.URL + f'country/{iso}/clubs','cheak the name'))['clubs']
         
     @staticmethod
-    def get_country_chess_com_name() -> str:
-        return json.loads(get_req(GetChessInfo.URL + 'country/','error'))['name']
+    def get_country_name_chess_com_name(iso):
+        return json.loads(get_req(GetChessInfo.URL + f'country/{iso}','error'))['name']
     
     @staticmethod
-    def get_online_streamers() -> None:
+    def get_online_streamers() -> dict:
         
         data : list[dict] = json.loads(get_req(GetChessInfo.URL + 'streamers/','error'))['streamers']
         online_streamers : dict = dict()
@@ -50,17 +54,7 @@ class GetChessInfo:
                 online_streamers[streamer['username']] = streamer['twitch_url']
         
         return online_streamers
-        
-    @staticmethod
-    def save_output(fname : str, data : dict) -> None:
-        if data == None:
-            print(clr.RED + 'No data')
-            
-        with open(f'{fname}.txt','a+') as af:
-            for val , key in data.items():
-                af.write(f'{str(key)} : {str(val)} \n')
-                af.close()
-    
+
     @staticmethod   
     def save_output(fname : str, data : list) -> None:
         
@@ -74,9 +68,4 @@ class GetChessInfo:
             af.write(f'time : {datetime.datetime.now} \n')
             for info in data:
                 af.write(f'{index}) {str(info)} \n')
-                af.close()
-                
-    
-    
-        
-        
+                af.close()        
